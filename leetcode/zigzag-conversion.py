64e4dd6fc80d52c04aa05f05e5db1b9b498adc33
boyka <upcvagen@163.com>
2020-07-25 00:27:41 +0800

    leetcode

--- leetcode/zigzag-conversion.py
+++ leetcode/zigzag-conversion.py
@@ -7,9 +7,46 @@
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        return
+        result = ""
+        length = len(s)
+        if numRows <= 0:
+            return result
+        elif numRows == 1 or length <= numRows:
+            return s
+        elif numRows == 2:
+            result = s[0::2] + s[1::2]
+            return result
+        else:
+            group = 2 * numRows - 2
+            for i in range(numRows):
+                if i == 0 or i == numRows - 1:
+                    result += s[i::group]
+                    # tag = i
+                    # while tag <= length - 1:
+                    #     result += s[tag]
+                    #     tag += group
+
+                else:
+
+                    # tag = 2 * numRows - i - 2
+                    # result += s[i,tag::group]
+                    # while tag <= length - 1:
+                    #     result += s[tag]
+                    #     tag += group
+
+                    tag1 = i
+                    tag2 = 2 * numRows - i - 2
+                    while tag1 <= length - 1 or tag2 <= length - 1:
+                        if tag1 <= length - 1:
+                            result += s[tag1]
+                            tag1 += group
+                        if tag2 <= length - 1:
+                            result += s[tag2]
+                            tag2 += group
+            # print(result)
+            return result
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.convert(""))
+    print("LDREOEIIECIHNTSG" == solution.convert("LEETCODEISHIRING", 4))
