2d5007009c90732f8d30608b733c1326075c79a2
boyka <upcvagen@163.com>
2020-07-24 23:24:04 +0800

    leetcode

---
+++ leetcode/string-to-integer-atoi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/23 下午2:55
+# @Author  : Boyka
+# @Email   : upcvagen@163.com
+# @Software: PyCharm
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        INT_MAX = pow(2, 32)
+        INT_MIN = pow(-2, 31)
+        return
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.longestPalindrome(""))
---
+++ leetcode/zigzag-conversion.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/24 下午11:14
+# @Author  : Boyka
+# @Email   : upcvagen@163.com
+# @Software: PyCharm
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        return
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.convert(""))
