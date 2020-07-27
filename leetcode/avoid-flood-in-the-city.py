a1fe6214dc64f7d58a0a07a9070ff3731adb155c
boyka <upcvagen@163.com>
2020-07-27 13:17:00 +0800

    leetcode

--- leetcode/avoid-flood-in-the-city.py
+++ leetcode/avoid-flood-in-the-city.py
@@ -5,6 +5,40 @@
 # @Email   : upcvagen@163.com
 # @Software: PyCharm
 
+# class Solution:
+#     def avoidFlood(self, rains):
+#         result = [1 for _ in range(len(rains))]
+#         sun_list = []
+#         rain_dict = dict()
+#
+#         for day, rain in enumerate(rains):
+#             if rain > 0:
+#                 result[day] = -1
+#                 if not rain in rain_dict:
+#                     rain_dict[rain] = [day]
+#                 else:
+#                     rain_dict[rain] = [rain_dict[rain][0], day]
+#             elif rain == 0:
+#                 sun_list.append(day)
+#             else:
+#                 return []
+#         for lake in rain_dict:
+#             if len(rain_dict[lake]) == 1:
+#                 continue
+#             else:
+#                 for start_id in range(int(len(rain_dict[lake]) / 2)):
+#                     start = rain_dict[lake][2 * start_id]
+#                     end = rain_dict[lake][2 * start_id + 1]
+#                     if_drain = False
+#                     for sun in sun_list:
+#                         if start < sun < end:
+#                             result[sun] = lake
+#                             if_drain = True
+#                             sun_list.remove(sun)
+#                             break
+#                     if not if_drain:
+#                         return []
+#         return result
 class Solution:
     def avoidFlood(self, rains):
         result = [1 for _ in range(len(rains))]
@@ -15,32 +49,23 @@ class Solution:
             if rain > 0:
                 result[day] = -1
                 if not rain in rain_dict:
-                    rain_dict[rain] = [day]
+                    rain_dict[rain] = day
                 else:
-                    rain_dict[rain] = [rain_dict[rain][0], day]
+                    for sun in sun_list:
+                        if sun > rain_dict[rain]:
+                            result[sun] = rain
+                            rain_dict[rain] = day
+                            sun_list.remove(sun)
+                            break
+                    else:
+                        return []
             elif rain == 0:
                 sun_list.append(day)
             else:
                 return []
-        for lake in rain_dict:
-            if len(rain_dict[lake]) == 1:
-                continue
-            else:
-                for start_id in range(int(len(rain_dict[lake]) / 2)):
-                    start = rain_dict[lake][2 * start_id]
-                    end = rain_dict[lake][2 * start_id + 1]
-                    if_drain = False
-                    for sun in sun_list:
-                        if start < sun < end:
-                            result[sun] = lake
-                            if_drain = True
-                            sun_list.remove(sun)
-                            break
-                    if not if_drain:
-                        return []
         return result
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.avoidFlood([69,0,0,0,69]))
+    print(solution.avoidFlood([1, 2, 0, 2, 3, 0, 1]))
