bdac419c2addc1dae9c2e40495a05abb3b6396e6
boyka <upcvagen@163.com>
2020-08-03 23:48:09 +0800

    leetcode

---
+++ leetcode/implement-queue-using-stacks-lcci.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/3 下午11:46
+# @Author  : Boyka
+# @Email   : upcvagen@163.com
+# @Software: PyCharm
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
