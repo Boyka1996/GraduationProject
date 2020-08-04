7421a26ddd75fc457c4adaa597c04e30adbf76a9
boyka <upcvagen@163.com>
2020-08-04 23:58:00 +0800

    leetcode

--- leetcode/implement-queue-using-stacks-lcci.py
+++ leetcode/implement-queue-using-stacks-lcci.py
@@ -11,32 +11,44 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
-
+        self.in_stack = []
+        self.out_stack = []
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-
+        self.in_stack.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
-
+        if len(self.out_stack) == 0:
+            while len(self.in_stack) != 0:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
 
     def peek(self) -> int:
+
         """
         Get the front element.
         """
-
+        if len(self.in_stack) == 0 and len(self.out_stack) == 0:
+            return 0
+        elif len(self.in_stack) == 0:
+            return self.out_stack[-1]
+        elif len(self.out_stack) == 0:
+            return self.in_stack[0]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-
-
+        if len(self.in_stack) == 0 and len(self.out_stack) == 0:
+            return True
+        else:
+            return False
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
