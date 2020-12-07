ca8d9db57a286236e1837c7aeed588db7f5f7438
Boyka <upcvagen@163.com>
2020-12-07 23:43:50 +0800

    something modified

--- dataset/annotation_to_heatmap/show_density_map.py
+++ dataset/annotation_to_heatmap/show_density_map.py
@@ -10,12 +10,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import json
+"""
 
-plt.imshow(Image.open('/home/chase/datasets/crowd_counting/ShanghaiTech/part_A_final/train_data/images/IMG_1.jpg'))
-plt.show()
+"""
+# plt.imshow(Image.open('/home/chase/datasets/crowd_counting/ShanghaiTech/part_A_final/train_data/images/IMG_1.jpg'))
+# plt.show()
 gt_file = np.load('/home/chase/datasets/crowd_counting/ShanghaiTech/part_A_final/train_data/density_map/IMG_1.npy')
-plt.imshow(gt_file, cmap=plt.cm.jet)
+plt.imshow(gt_file,cmap=plt.cm.jet)
+# plt.imshow(gt_file)
+
 plt.show()
+
 
 print(np.sum(gt_file))
 with open('/home/chase/datasets/crowd_counting/ShanghaiTech/part_A_final/train_data/json/IMG_1.json', 'r') as fr:
